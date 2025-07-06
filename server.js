fbd9cd2c1d07ddb15877d5a37f4c1847ae04533a
mhmtkts <mahmutaktas.m@gmail.com>
2025-07-06 04:53:08 +0300

    feat: add product retrieval endpoint and update welcome message

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
+const { count } = require('console');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,8 +10,35 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+const getProducts = () => {
+    try{
+        const data = fs.readFileSync(path.join(__dirname, 'products.json'), 'utf8');
+        return JSON.parse(data);
+    } catch (error) {
+        console.error('JSON dosyası okunamadı:', error);
+        return [];
+    }
+}
+
 app.get('/', (req, res) => {
-    res.json({ message: 'Welcome to the Renart Case API!' });
+    res.json({ message: 'API çalışıyor!' });
+});
+
+app.get('/api/products', (req, res) => {
+    try {
+        const products = getProducts();
+        res.json({
+            success: true,
+            data: products,
+            count: products.length
+        });
+        
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            error: 'Ürünler alınamadı.'
+        });
+    }
 });
 
 app.listen(PORT, () => {
