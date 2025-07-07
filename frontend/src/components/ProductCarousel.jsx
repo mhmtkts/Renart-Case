5ff5bbd10e1330d68e8bcca92807b066aba39736
mhmtkts <mahmutaktas.m@gmail.com>
2025-07-08 00:43:20 +0300

    style: increase arrow size and adjust positioning in ProductCarousel for better visibility

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -16,7 +16,7 @@ const PrevArrow = (props) => {
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        className="h-6 w-6"
+        className="h-10 w-10"
         fill="none"
         viewBox="0 0 24 24"
         stroke="currentColor"
@@ -42,7 +42,7 @@ const NextArrow = (props) => {
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        className="h-6 w-6"
+        className="h-10 w-10"
         fill="none"
         viewBox="0 0 24 24"
         stroke="currentColor"
